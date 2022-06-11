8df8657ebe7ade18b8e34dcc6dff64d13c11ad08
Mychael Dourado <mychaeldourado23@gmail.com>
2022-06-11 18:15:18 -0300

    All services.

---
+++ server/service/render.js
@@ -0,0 +1,39 @@
+const axios = require('axios');
+
+const home = async (req, res) => {
+
+  try {
+
+    const response = await axios.get("http://localhost:4000/api/users")
+
+    const userData = response.data
+
+    res.render('index', { user: userData })
+
+  } catch (error) {
+    console.log(error)
+  }
+
+
+}
+
+const addUser = (req, res) => {
+
+  res.render('add-user')
+
+}
+
+const updateUser = async (req, res) => {
+
+  const response = await axios.get("http://localhost:4000/api/users/", { params: { id: req.query.id } })
+
+  const userData = response.data
+  res.render('update-user', { user: userData })
+
+}
+
+module.exports = {
+  home,
+  addUser,
+  updateUser
+}
